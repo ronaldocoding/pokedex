a6ca628f478012dfc5b487bfe8f42f8a0a7cecfe
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-11-09 16:11:44 -0400

    Add PokemonRepository parameter and usage to PokemonRepositoryImpl

--- app/src/main/java/br/com/pokedex/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/br/com/pokedex/data/repository/PokemonRepositoryImpl.kt
@@ -1,17 +1,10 @@
 package br.com.pokedex.data.repository
 
-import br.com.pokedex.api.PokemonApi
+import br.com.pokedex.data.api.PokemonApi
 import br.com.pokedex.data.mapper.toModel
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import br.com.pokedex.domain.repository.PokemonRepository
 
-class PokemonRepositoryImpl {
+class PokemonRepositoryImpl(private val api: PokemonApi) : PokemonRepository {
 
-    private val api: PokemonApi = Retrofit.Builder()
-        .baseUrl("https://pokeapi.co/api/v2/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(PokemonApi::class.java)
-
-    suspend fun getSinglePokemon(id: Int) = api.getSinglePokemon(id).toModel()
+    override suspend fun getSinglePokemon(id: Int) = api.getSinglePokemon(id).toModel()
 }

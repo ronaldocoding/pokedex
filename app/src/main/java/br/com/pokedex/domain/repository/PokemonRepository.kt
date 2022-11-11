7de45831177343a596d634a1d8c1856bb20ec383
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-11-11 11:29:35 -0400

    Remove Resource class

--- app/src/main/java/br/com/pokedex/domain/repository/PokemonRepository.kt
+++ app/src/main/java/br/com/pokedex/domain/repository/PokemonRepository.kt
@@ -1,8 +1,6 @@
 package br.com.pokedex.domain.repository
 
 import androidx.paging.PagingData
-import br.com.pokedex.data.api.Resource
-import br.com.pokedex.data.api.dto.SinglePokemonDTO
 import br.com.pokedex.domain.model.SinglePokemon
 import kotlinx.coroutines.flow.Flow
 

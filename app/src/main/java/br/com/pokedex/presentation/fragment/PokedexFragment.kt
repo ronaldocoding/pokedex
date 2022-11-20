e12d96f131a73f60604ec005375307eb322b9af0
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-11-20 14:22:30 -0400

    Finish search bar visual

--- app/src/main/java/br/com/pokedex/presentation/fragment/PokedexFragment.kt
+++ app/src/main/java/br/com/pokedex/presentation/fragment/PokedexFragment.kt
@@ -2,6 +2,7 @@ package br.com.pokedex.presentation.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,6 +22,7 @@ import br.com.pokedex.presentation.viewmodel.PokedexViewModel
 import br.com.pokedex.util.Constants.ONE_SPAN_SIZE
 import br.com.pokedex.util.Constants.POKEMON_VIEW_TYPE
 import br.com.pokedex.util.Constants.TWO_SPANS_SIZE
+import br.com.pokedex.util.emptyString
 import br.com.pokedex.util.hideView
 import br.com.pokedex.util.showView
 import kotlinx.coroutines.flow.collectLatest
@@ -142,20 +144,24 @@ class PokedexFragment : Fragment() {
     }
 
     private fun setUpSearchBar(view: View) {
-        binding.textInputEditText.apply {
-            setOnTouchListener { _, _ ->
-                isCursorVisible = true
-                performClick()
-            }
-
-            setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    hideKeyboard(view)
-                    isCursorVisible = false
-                    return@OnEditorActionListener true
+        binding.apply {
+            textInputEditText.apply {
+                setOnTouchListener { _, _ ->
+                    isCursorVisible = true
+                    performClick()
                 }
-                false
-            })
+                setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        hideKeyboard(view)
+                        isCursorVisible = false
+                        return@OnEditorActionListener true
+                    }
+                    false
+                })
+            }
+            searchBar.setOnClickListener {
+                showKeyboard()
+            }
         }
     }
 
@@ -164,8 +170,15 @@ class PokedexFragment : Fragment() {
         currentView?.let {
             val imm =
                 requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
+            imm.hideSoftInputFromWindow(binding.textInputEditText.windowToken, 0)
         }
+    }
+
+    private fun FragmentPokedexBinding.showKeyboard() {
+        textInputEditText.requestFocus()
+        textInputEditText.isCursorVisible = true
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(textInputEditText, 0)
     }
 
     private fun getPokemon() {
